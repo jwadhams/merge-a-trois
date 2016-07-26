d2db32b5f6e4cafb2a31d8c2b7bacd2d10e596cd
Jeremy Wadhams <jwadhams@wingtiplabs.com>
2016-07-26 00:48:47 -0500

    Cleaning out cruft

--- src/JWadhams/MergeATrois.php
+++ src/JWadhams/MergeATrois.php
@@ -67,39 +67,6 @@ class MergeATrois{
         }
       }
     }
-  	// if isArray
-  	// 	a = [] if not Array.isArray a
-  	// 	o = [] if not Array.isArray o
-    //
-  	// 	for k of a
-  	// 		unless a[k] not in b and a[k] in o
-  	// 			result.push a[k]
-    //
-  	// 	for k of b
-  	// 		if not k of a
-  	// 			result.push b[k]
-  	// 		else if typeof a[k] is 'object' and typeof b[k] is 'object'
-  	// 			ov = if k of o and typeof o[k] is 'object' then o[k] else {}
-  	// 			result[k] = merge ov, a[k], b[k]
-  	// 		else if b[k] not in a
-  	// 			result.push b[k]
-    //
-  	// else
-  	// 	a = {} if Array.isArray a
-    //
-  	// 	for k of b
-  	// 		result[k] = b[k]
-    //
-  	// 	for k of a
-  	// 		if not k of result
-  	// 			result[k] = a[k]
-  	// 		else if a[k] isnt result[k]
-  	// 			if typeof a[k] is 'object' and typeof b?[k] is 'object'
-  	// 				ov = if o? and k of o and typeof o[k] is 'object' then o[k] else {}
-  	// 				result[k] = merge ov, a[k], b[k]
-  	// 			else if b?[k] is o?[k]
-  	// 				result[k] = a[k]
-    //
   	return $result;
   }
 
